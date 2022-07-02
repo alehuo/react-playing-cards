c2da74f84f43c074a020651090e9b7fd009d51c7
Aleksi Huotala <aleksi.huotala@reaktor.com>
2022-07-02 10:10:31 +0300

    Add .nvmrc
    Transform to TypeScript, use Parcel to build library. Update Storybook and use TS with Storybook.

---
+++ src/PlayingCard.tsx
@@ -0,0 +1,68 @@
+import classnames from "classnames";
+import React from "react";
+import * as styles from "./styles/PlayingCard.module.scss";
+import * as suitStyles from "./styles/Suits.module.scss";
+import type { Suit } from "./types";
+
+type PlayingCardProps = {
+  value: number;
+  suit: Suit;
+} & React.HTMLAttributes<HTMLDivElement>;
+
+const resolveValue = (val: number): string | number => {
+  switch (val) {
+    case 1:
+      return "A";
+    case 11:
+      return "J";
+    case 12:
+      return "Q";
+    case 13:
+      return "K";
+    default:
+      return val;
+  }
+};
+
+const resolveSuitFragment = (suit: Suit) => {
+  switch (suit) {
+    case "spades":
+      return <>&spades;</>;
+    case "diamonds":
+      return <>&diams;</>;
+    case "hearts":
+      return <>&hearts;</>;
+    case "clubs":
+      return <>&clubs;</>;
+  }
+};
+
+export const PlayingCard: React.FC<PlayingCardProps> = ({
+  value,
+  suit,
+  ...rest
+}) => {
+  const resolvedValue = resolveValue(value);
+  const resolvedSuit = resolveSuitFragment(suit);
+
+  return (
+    <div
+      className={classnames(styles.playingCard, suitStyles[`${suit}`])}
+      {...rest}
+    >
+      <div className={styles["playingCard__valueUpperLeft"]}>
+        {resolvedValue}
+      </div>
+      <div className={styles["playingCard__valueLowerRight"]}>
+        {resolvedValue}
+      </div>
+      <div className={styles["playingCard__suitCenter"]}>
+        {resolvedSuit}
+      </div>
+      <div className={styles["playingCard__suitUpperLeft"]}>{resolvedSuit}</div>
+      <div className={styles["playingCard__suitLowerRight"]}>
+        {resolvedSuit}
+      </div>
+    </div>
+  );
+};
